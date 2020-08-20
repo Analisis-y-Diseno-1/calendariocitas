2961400e50c7a8c5b0bf893bb1b311b31e13f91c
Anthony Chitay <antonchitay@icloud.com>
2020-08-20 14:05:15 -0600

    agregando tablas

--- users/models.py
+++ users/models.py
@@ -1,5 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from phone_field import PhoneField
 
 class CustomUser(AbstractUser):
     pass
+
+
+# PK -- ID
+# NOmbre paciente
+# telefono
+# correo
+# fecha de nacimiento
+# sexo
+# direccion
+# Observacion(alergias o algo asi)
+class Paciente(models.Model):  
+
+    MASCULINO = "M"
+    FEMENINO = "F"
+    
+    SEXO_CHOICES = [
+        (MASCULINO, 'Masculino'),
+        (FEMENINO, 'Femenino'),
+    ]
+    nombre = models.CharField(max_length=250, blank=False, null=False)
+    telefono = PhoneField(blank=True, help_text="Numero telefonico de contacto")
+    correo = models.EmailField(max_length=254, blank=False, null=False)
+    fecha_de_nacimiento = models.DateField(auto_now=False, auto_now_add=False)
+    fecha_de_registro = models.DateTimeField(auto_now=False, auto_now_add=True)
+    fecha_de_modificacion = models.DateTimeField(auto_now=True, auto_now_add=False)
+    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES, default=MASCULINO)
+    direccion = models.CharField(max_length=250)
+    observacion = models.CharField(max_length=250)
+
+
+    class Meta:
+        verbose_name = "Paciente"
+        verbose_name_plural = "Pacientes"
+
+    def __str__(self):
+        return self.nombre
+
+    def get_absolute_url(self):
+        return reverse("Paciente_detail", kwargs={"pk": self.pk})
--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,56 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from django.contrib.auth import get_user_model
+from .models import Paciente
+import datetime
+
+
+class PacienteTests(TestCase):
+
+
+    def setUp(self):
+        Paciente.objects.create(
+            nombre="Brandon Chitay",
+            telefono="+50247529761",
+            correo="antonchitay@gmail.com",
+            sexo="M",
+            direccion="Planeta Tierra",
+            observacion="Vivo en la tierra",
+            fecha_de_nacimiento="1996-11-07"
+        )
+
+    def test_create_paciente(self):
+        paciente = Paciente.objects.create(
+            nombre="Brandon Chitay",
+            telefono="+50247529761",
+            correo="antonchitay@gmail.com",
+            sexo="M",
+            direccion="Planeta Tierra",
+            observacion="Vivo en la tierra",
+            fecha_de_nacimiento="1996-11-07"
+        )
+
+        self.assertEqual(paciente.telefono, "+50247529761")
+        self.assertEqual(paciente.correo,"antonchitay@gmail.com")
+        self.assertEqual(paciente.sexo, "M")
+        self.assertEqual(paciente.direccion, "Planeta Tierra")
+        self.assertEqual(paciente.observacion, "Vivo en la tierra")
+        self.assertEqual(paciente.fecha_de_nacimiento, '1996-11-07')
+        
+    def test_paciente_exist(self):
+        paciente = Paciente.objects.filter(nombre="Brandon Chitay")
+        self.assertEqual(len(paciente), 1)
+
+    def test_paciente_does_not_exist(self):
+        paciente = Paciente.objects.filter(nombre="Brandon Anthony")
+        self.assertEqual(len(paciente), 0)
+    
+    def test_paciente_modified(self):
+        Paciente.objects.filter(nombre="Brandon Chitay").update(nombre="Anthony Chitay")
+        paciente = Paciente.objects.get(nombre="Anthony Chitay")
+        self.assertEqual(paciente.telefono, "+50247529761")
+
+
 class CustomUserTests(TestCase):
 
     def test_create_user(self):

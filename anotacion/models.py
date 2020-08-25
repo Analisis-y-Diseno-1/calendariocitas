a5b0acf1d3fd9d047668468bde73895ee4ada18e
HenryLeon95 <henriscoh1995@gmail.com>
2020-08-25 16:18:20 -0600

    Agregando modelo

--- anotacion/models.py
+++ anotacion/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime 
 
 # Create your models here.
+class Anotacion(models.Model):
+    no_anotacion = models.AutoField(primary_key=true)
+    id_cita = models.IntegerField(blank=true, null=true) #Cambiar esto porque hay que esperar la tabla cita
+    descripcion = models.CharField(max_length=200)
+    fecha_hora = models.DateTimeField(default=datetime.now)
+

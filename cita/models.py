d7ea76a87ee0cbb5157b1965eff539177bb05954
CarlosSalguero <carlos.bautista2008@gmail.com>
2020-08-26 12:26:54 -0600

    Se agrega campo fecha para registrar citas y campo paciente para relacionar pacientes con cita

--- cita/models.py
+++ cita/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from anotacion.models import Anotacion
+#from anotacion.models import Anotacion
 
 # Create your models here.
 class Cita(models.Model):
-    fecha = models.DateField()
-    hora = models.TimeField()
+    fecha = models.DateTimeField()
+    fecha_cita = models.DateField()
+    hora_cita = models.TimeField()
     estado = models.CharField(max_length=10)
     comentario = models.CharField(max_length=50)
-    anotacion = models.ForeignKey(Anotacion, on_delete=models.CASCADE)
+    paciente = models.IntegerField()

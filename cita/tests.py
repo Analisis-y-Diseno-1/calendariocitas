d5002c837a6e81445a3a0d211d77fc60f0c1bf8c
HenryLeon95 <henriscoh1995@gmail.com>
2020-08-26 21:39:32 -0600

    Realizando prueba unitaria

--- cita/tests.py
+++ cita/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Cita
+from datetime import datetime, timedelta, date
 
 # Create your tests here.
+
+class ModelCitaTest(TestCase):
+
+    def test_create_city(self):
+        fecha = datetime.now()
+        hora = datetime.time()
+        Cita(fecha = fecha, fecha_cita = fecha, hora_cita=hora, estado='Pendiente', comentario='prueba', paciente=1001).save()
+
+        citas = Cita.objects.all()
+        self.assertEqual(citas[0].fecha, fecha)
+        self.assertEqual(citas[0].fecha_cita, fecha)
+        self.assertEqual(citas[0].hora_cita, hora)
+        self.assertEqual(citas[0].comentario, 'prueba')
+        self.assertEqual(citas[0].paciente, 1001)
+        self.assertEqual(citas[0].estado, 'Pendiente')

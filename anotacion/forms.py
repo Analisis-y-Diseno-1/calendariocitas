0f63de0b1da7673b661c0e665e076e8677dc57ed
HenryLeon95 <henriscoh1995@gmail.com>
2020-08-27 10:36:18 -0600

    Configurando forms

--- anotacion/forms.py
+++ anotacion/forms.py
@@ -1,4 +1,17 @@
-
 from django import forms
+from models import Anotacion
 
 #from anotacion.models import models
+class AnotacionForm(forms.ModelForm):
+    class Meta:
+        model = Anotacion
+        fields = [
+            'descripcion',
+        ]
+        labels = {
+            'descripcion': 'Descripción',
+        }
+        widgets = {
+            'descripcion': forms.Textarea(attrs={'class': 'form_control', 'cols': 15, 'rows': 3}),
+        }
+

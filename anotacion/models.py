cb2f1a514898e02af7d2913622f533f4a123bc42
HenryLeon95 <henriscoh1995@gmail.com>
2020-08-26 22:28:48 -0600

    Realizando relacion anotacion-cita

--- anotacion/models.py
+++ anotacion/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from datetime import datetime 
+from ..cita.models import Cita
 
 # Create your models here.
 class Anotacion(models.Model):
     no_anotacion = models.AutoField(primary_key=True)
-    id_cita = models.IntegerField(blank=True, null=True) #Cambiar esto porque hay que esperar la tabla cita
+    id_cita = models.ForeignKey('Cita', db_column='id_cita', on_delete=models.CASCADE)
     descripcion = models.CharField(max_length=200)
-    fecha_hora = models.DateTimeField(default=datetime.now)
+    fecha_hora = models.DateTimeField(default=datetime.no
 

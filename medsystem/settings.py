baa71c27fe4c12f7e615437e6a9f28b44ea63d3e
Anthony Chitay <antonchitay@icloud.com>
2020-08-28 18:38:43 -0600

    Agregando la vista para seleccionar cita

--- medsystem/settings.py
+++ medsystem/settings.py
@@ -142,6 +142,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 
 # Custom user settings
 AUTH_USER_MODEL = 'users.CustomUser'
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from cita.models import Cita
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class AppointmentsListView(ListView):
+    model = Cita
+    context_object_name = 'cita_list'
+    template_name = 'citas/lista_citas.html'
+
+
+class AppointmentDetailView(DetailView):
+    model = Cita
+    context_object_name = 'cita'
+    template_name = 'citas/cita_detail.html'

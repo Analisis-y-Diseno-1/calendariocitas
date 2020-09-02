77b9e52caa6b3f16b3d32ca3e90810e14a129f69
Anthony Chitay <antonchitay@icloud.com>
2020-09-02 15:35:03 -0600

    agregando detalle de la cita

--- pages/urls.py
+++ pages/urls.py
@@ -5,7 +5,7 @@ from anotacion.views import crear_anotacion,modificar_anotacion,listado_anotacio
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('citas', AppointmentsListView.as_view(), name='citas'),
-    path('citas/<int:pk>', AppointmentsListView.as_view(), name='cita_detail'),
+    path('citas/<int:pk>', AppointmentDetailView.as_view(), name='cita_detail'),
     path('anotacion/', crear_anotacion, name='anotacion'),
     path('listado_anotaciones/', listado_anotaciones, name='listado_anotaciones'),
     path('modificar_anotacion/<id>', modificar_anotacion, name='modificar_anotacion'),

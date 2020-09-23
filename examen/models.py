60b5523483f0b326ab0b345cb4013c93b5062f08
Henry León <henriscoh1995@gmail.com>
2020-09-22 19:48:27 -0600

    Se agrego la funcion __str__ para obtener la descripcion del examen y paso la prueba

--- examen/models.py
+++ examen/models.py
@@ -6,3 +6,6 @@ class Examen(models.Model):
     fecha = models.DateTimeField(auto_now_add=True)
     descripcion = models.CharField(max_length=300)
     cita = models.ForeignKey(Cita, related_name="examenes", on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.descripcion

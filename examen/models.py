00d2cda7bcc95ff8701c83a6f3fd09775d1375aa
Henry León <henriscoh1995@gmail.com>
2020-09-22 19:07:56 -0600

    Generando el modelo para examenes clinicos

---
+++ examen/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.urls import reverse
+from cita.models import Cita
+
+class Examen(models.Model):
+    fecha = models.DateTimeField(auto_now_add=True)
+    descripcion = models.CharField(max_length=300)
+    cita = models.ForeignKey(Cita, related_name="examenes", on_delete=models.CASCADE, blank=True, null=True)

bc8f101d89bb0d4e769f394e7b6b9195809d9a9a
branthonyc <antonchitay@icloud.com>
2020-09-26 11:44:38 -0600

    Modificar cita completo

--- pages/views.py
+++ pages/views.py
@@ -58,6 +58,11 @@ class AppointmentDetailView(DetailView):
         })
 
         return context
+    
+class AppointmentUpdateView(DetailView):
+    model = Cita
+    context_object_name = 'cita'
+    template_name = 'citas/actualizar_cita.html'
 
 class AppointmentCreate(TemplateView):
     template_name = 'citas/crear_cita.html'
@@ -103,22 +108,22 @@ class SearchResultsListView(ListView):
 
         return context
 
-def modificar_cita(request, pk):
-    query = request.POST
-    if Cita.objects.filter(id=pk).exists():
-        try:
-            cita = Cita.objects.get(id=pk)
-            cita.fecha_cita=query['fecha_cita']
-            cita.hora_cita=query['hora_cita']
-            cita.comentario=query['comentario']
+# def modificar_cita(request, pk):
+#     query = request.POST
+#     if Cita.objects.filter(id=pk).exists():
+#         try:
+#             cita = Cita.objects.get(id=pk)
+#             cita.fecha_cita=query['fecha_cita']
+#             cita.hora_cita=query['hora_cita']
+#             cita.comentario=query['comentario']
 
-            cita.save()
-        except:
-            messages.add_message(request, messages.ERROR,'Error, No se pudo actualizar cita')
+#             cita.save()
+#         except:
+#             messages.add_message(request, messages.ERROR,'Error, No se pudo actualizar cita')
     
-        else:
-            messages.add_message(request, messages.INFO,'Se realizaron los cambios con exito!')
+#         else:
+#             messages.add_message(request, messages.INFO,'Se realizaron los cambios con exito!')
     
-    return redirect('citas/crear_cita.html',pk=pk)
+#     return redirect('citas/actualizar_cita.html',pk=pk)
             
     

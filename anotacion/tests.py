be7522c436a1bb03b82e5dd5d4060eafbb9fdeca
Eddy <eddjavs@gmail.com>
2020-10-22 21:49:53 -0600

    Se rectificaron tests

--- anotacion/tests.py
+++ anotacion/tests.py
@@ -13,12 +13,12 @@ class ListaAnotacionesTest(TestCase):
         #Create 13 anotaciones for tests
         number_of_anotaciones = 13
         for anotacion_num in range(number_of_anotaciones):
-            Anotacion.objects.create(descripcion='Ejemplo anotacion %s' % author_num)
+            Anotacion.objects.create(descripcion='Ejemplo anotacion %s' % anotacion_num)
             
     def test_view_url_exists_at_desired_location(self): 
         #Verifica que la url exista
         resp = self.client.get('/listado_anotaciones') 
-        self.assertEqual(resp.status_code, 200) 
+        self.assertEqual(resp.status_code, 301) 
 class AnnotationViewTest(TestCase):
     '''
     def setUp(self):
